b6a56f365aa98c0b1f22ac7d696138c11726c1ce
Thomas Shamp <T.shamp7@gmail.com>
2020-11-11 23:58:32 -0800

    implement burger menu slideout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,58 @@
 import React from "react";
 import backgroundOne from "../resources/images/pexels-bri-schneiter-346529.jpg";
-import BootstrapCarousel from "./BootstrapCarousel";
 
 const Main = () => {
   const topStyle = {
-    width: "auto",
+    width: "100%",
     height: "100vh",
-    marginLeft: "300px",
+    marginTop: "-30px",
     background: `url(${backgroundOne})`,
     backgroundSize: "cover",
     display: "flex",
     alignItems: "center",
+    justifyContent: "center",
     backgroundRepeat: "no-repeat",
-    backgroundPosition: "bottom",
+    backgroundPosition: "center",
+    overflow: "hidden",
   };
 
   return (
-    <div style={topStyle}>
-      <BootstrapCarousel />
+    <div className="main-container">
+      <div style={topStyle}>
+        <div className="banner-container">
+          <div className="banner-contents">Hi!</div>
+          <div className="banner-contents">I'm Tom Shamp!</div>
+        </div>
+      </div>
+      <div className="subsection-container">
+        <div className="subsection">
+          <h6>About</h6>
+          <p>Some information about me</p>
+        </div>
+      </div>
+
+      <div className="subsection-container">
+        <div className="subsection">
+          <h1>Projects</h1>
+          <p>
+            Here is a list of technologies which are showcased in my various
+            projects! Select one to find out more!
+          </p>
+          <ul>
+            <li>Ruby / Rails projects</li>
+            <li>React Projects</li>
+            <li>MERN stack</li>
+            <li>Client Projects</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="subsection-container">
+        <div className="subsection">
+          <h1>Contact Me</h1>
+          <p>Want to get in touch? Send me an email here!</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,40 +4,91 @@ import LinkItem from "./LinkItem";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar-container">
-      <img src={resumePic} alt="profile" className="profile-pic" />
-      <LinkItem value="Tom Shamp" />
-      <br />
-      <div>
-        <i className="fas fa-envelope"></i>
-        <LinkItem value="T.shamp7@gmail.com" />
-      </div>
-      <br />
-      <div>
-        <i className="fas fa-phone-alt"></i>
-        <LinkItem value="(401) 678-1546" />
-      </div>
-      <br />
-      <LinkItem value="INTRODUCTION" />
-      <br />
-      <LinkItem value="ABOUT" />
-      <br />
-      <LinkItem value="PROJECTS" />
-      <br />
-      <LinkItem value="CONTACT ME" />
-      <br />
-      <a className="btn" href="https://github.com/Tshamp7">
-        {" "}
-        <i className="fab fa-github fa-lg"></i>{" "}
-      </a>
-      <br />
-      <a
-        className="btn"
-        href="https://www.linkedin.com/in/thomas-shamp-a1207b158/"
-      >
-        {" "}
-        <i className="fab fa-linkedin fa-lg"></i>{" "}
-      </a>
+    // <div className="sidebar-container">
+    //   <img src={resumePic} alt="profile" className="profile-pic" />
+    //   <LinkItem value="Tom Shamp" />
+    //   <br />
+    //   <div>
+    //     <i className="fas fa-envelope"></i>
+    //     <LinkItem value="T.shamp7@gmail.com" />
+    //   </div>
+    //   <br />
+    //   <div>
+    //     <i className="fas fa-phone-alt"></i>
+    //     <LinkItem value="(401) 678-1546" />
+    //   </div>
+    //   <br />
+    //   <LinkItem value="INTRODUCTION" />
+    //   <br />
+    //   <LinkItem value="ABOUT" />
+    //   <br />
+    //   <LinkItem value="PROJECTS" />
+    //   <br />
+    //   <LinkItem value="CONTACT ME" />
+    //   <br />
+    //   <a className="btn" href="https://github.com/Tshamp7">
+    //     {" "}
+    //     <i className="fab fa-github fa-lg"></i>{" "}
+    //   </a>
+    //   <br />
+    //   <a
+    //     className="btn"
+    //     href="https://www.linkedin.com/in/thomas-shamp-a1207b158/"
+    //   >
+    //     {" "}
+    //     <i className="fab fa-linkedin fa-lg"></i>{" "}
+    //   </a>
+    // </div>
+
+    <div id="menuToggle">
+      <input type="checkbox" />
+
+      <span></span>
+      <span></span>
+      <span></span>
+
+      <ul id="menu">
+        <li>
+          <img src={resumePic} alt="profile" className="profile-pic" />
+          <a className="btn" href="https://github.com/Tshamp7">
+            {" "}
+            <i className="fab fa-github fa-lg"></i>{" "}
+          </a>
+          <a
+            className="btn"
+            href="https://www.linkedin.com/in/thomas-shamp-a1207b158/"
+          >
+            {" "}
+            <i className="fab fa-linkedin fa-lg"></i>{" "}
+          </a>
+        </li>
+        <li>
+          <p>
+            <LinkItem value="Tom Shamp" />
+          </p>
+        </li>
+        <li>
+          {" "}
+          <i className="fas fa-envelope"></i>
+          <LinkItem value="T.shamp7@gmail.com" />
+        </li>
+        <li>
+          <i className="fas fa-phone-alt"></i>
+          <LinkItem value="(401) 678-1546" />
+        </li>
+        <a href="#!">
+          <li>Introduction</li>
+        </a>
+        <a href="#!">
+          <li>About</li>
+        </a>
+        <a href="#!">
+          <li>Projects</li>
+        </a>
+        <a href="#!">
+          <li>Contact</li>
+        </a>
+      </ul>
     </div>
   );
 };
