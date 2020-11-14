6a628eccb50cc6e20be00d90f5ff70cba97ad36a
Thomas Shamp <T.shamp7@gmail.com>
2020-11-13 16:57:02 -0800

    fix menu icons and add twitter link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,47 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import resumePic from "../resources/images/resume-pic-example.jpeg";
 import LinkItem from "./LinkItem";
 
 const Sidebar = () => {
-  return (
-    // <div className="sidebar-container">
-    //   <img src={resumePic} alt="profile" className="profile-pic" />
-    //   <LinkItem value="Tom Shamp" />
-    //   <br />
-    //   <div>
-    //     <i className="fas fa-envelope"></i>
-    //     <LinkItem value="T.shamp7@gmail.com" />
-    //   </div>
-    //   <br />
-    //   <div>
-    //     <i className="fas fa-phone-alt"></i>
-    //     <LinkItem value="(401) 678-1546" />
-    //   </div>
-    //   <br />
-    //   <LinkItem value="INTRODUCTION" />
-    //   <br />
-    //   <LinkItem value="ABOUT" />
-    //   <br />
-    //   <LinkItem value="PROJECTS" />
-    //   <br />
-    //   <LinkItem value="CONTACT ME" />
-    //   <br />
-    //   <a className="btn" href="https://github.com/Tshamp7">
-    //     {" "}
-    //     <i className="fab fa-github fa-lg"></i>{" "}
-    //   </a>
-    //   <br />
-    //   <a
-    //     className="btn"
-    //     href="https://www.linkedin.com/in/thomas-shamp-a1207b158/"
-    //   >
-    //     {" "}
-    //     <i className="fab fa-linkedin fa-lg"></i>{" "}
-    //   </a>
-    // </div>
+  const toggleMenu = useRef(null);
 
+  const closeMenu = () => {
+    toggleMenu.current.checked = false;
+  };
+
+  return (
     <div id="menuToggle">
-      <input type="checkbox" />
+      <input type="checkbox" ref={toggleMenu} />
 
       <span></span>
       <span></span>
@@ -49,45 +19,59 @@ const Sidebar = () => {
 
       <ul id="menu">
         <li>
-          <img src={resumePic} alt="profile" className="profile-pic" />
-          <a className="btn" href="https://github.com/Tshamp7">
-            {" "}
-            <i className="fab fa-github fa-lg"></i>{" "}
-          </a>
-          <a
-            className="btn"
-            href="https://www.linkedin.com/in/thomas-shamp-a1207b158/"
-          >
-            {" "}
-            <i className="fab fa-linkedin fa-lg"></i>{" "}
-          </a>
+          <div id="menu-pic">
+            <img src={resumePic} alt="profile" className="profile-pic" />
+            <div id="social-media-links">
+              <a className="btn" href="https://github.com/Tshamp7">
+                {" "}
+                <i className="fab fa-github fa-lg"></i>{" "}
+              </a>
+              <a
+                className="btn"
+                href="https://www.linkedin.com/in/thomas-shamp-a1207b158/"
+              >
+                {" "}
+                <i className="fab fa-linkedin fa-lg"></i>{" "}
+              </a>
+              <a href="https://www.twitter.com/tom_shamp" className="btn">
+                <i class="fab fa-twitter-square fa-lg"></i>
+              </a>
+            </div>
+          </div>
         </li>
         <li>
-          <p>
-            <LinkItem value="Tom Shamp" />
-          </p>
+          <LinkItem value="Tom Shamp" />
         </li>
         <li>
-          {" "}
-          <i className="fas fa-envelope"></i>
-          <LinkItem value="T.shamp7@gmail.com" />
+          <div className="link-container">
+            <i className="fas fa-envelope"></i>
+            <LinkItem value="  T.shamp7@gmail.com" />
+          </div>
         </li>
         <li>
           <i className="fas fa-phone-alt"></i>
-          <LinkItem value="(401) 678-1546" />
+          <LinkItem value="  (401) 678-1546" />
         </li>
-        <a href="#introduction">
-          <li>Introduction</li>
-        </a>
-        <a href="#about">
-          <li>About</li>
-        </a>
-        <a href="#projects">
-          <li>Projects</li>
-        </a>
-        <a href="#contact">
-          <li>Contact</li>
-        </a>
+        <li>
+          <a href="#introduction" onClick={closeMenu}>
+            Introduction
+          </a>
+        </li>
+        <li>
+          <a href="#about" onClick={closeMenu}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#projects" onClick={closeMenu}>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#contact" onClick={closeMenu}>
+            Contact
+          </a>
+        </li>
       </ul>
     </div>
   );
